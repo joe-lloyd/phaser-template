e13cb3177f2f2da2a2ab0e99d9380570076b51e0
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2020-10-04 22:57:52 +0200

    lint configs fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parserOptions: {
     project: './tsconfig.json',
+    createDefaultProgram: true,
   },
 };
